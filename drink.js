93c5305bbc52328e73e8626ae520e64c495fa092
Daniel Vega <danielalexvega@gmail.com>
2020-07-05 15:11:13 -0500

    added bookshelf, barcart, and more goodies

--- drink.js
+++ drink.js
@@ -1,40 +1,44 @@
 
 var queryURL = "https://www.thecocktaildb.com/api/json/";
 var apiKey = "v2/9973533"
-var alcoholic = queryURL + apiKey + "/filter.php?a=Alcoholic"; // List of all the alcoholic drinks.
-var nonAlcoholic = queryURL + apiKey + "/filter.php?a=Non_Alcoholic"; // List of all the non-alcoholic drinks --- possibly for sober adults/underage.
-var drinkId = queryURL + apiKey + "/lookup.php?i=";// We will add the drink ID in the second $ajax call with var codeDrinkId = response.drinks[bookVariable#].idDrink (url: drinkId + codeDrinkId,) Do we need to change codeDrinkId into a string?
+var alcoholic = queryURL + apiKey + "/filter.php?a=Alcoholic";
+var nonAlcoholic = queryURL + apiKey + "/filter.php?a=Non_Alcoholic";
+var drinkId = queryURL + apiKey + "/lookup.php?i=";
 
 var drinkContainer;
-//first $ajax call // make this variable empty and put in the first $ajax call.
-var drinkIdNumber; // = response.drinks[drinkValue].idDrink;  // how will we connect the book.js value to the rest of the drink.js ?
-
-////second $ajax call ... make these empty variables and put in the second $ajax call.
-var drinkName; // = response.drinks.strDrink;
-var measurements; // = response.drinks.strMeasure(#1 -?); // how do we run through the #'s we want to include and exclude the null values? A for loop? 
-var ingredients; // = response.drinks.strIngredient(#1 -?); // how do we run through the #'s we want to include and exclude the null values? A for loop? 
-var drinkInstructions; // = repsonse.drinks.strInstructions;
+var drinkIdNumber; 
+var drinkName;
+var measurements;
+var ingredients;
+var drinkInstructions;
 var totalDrinkInfo;
+var drinkUrl;
+var drinkImg;
+var savedDrinkArr = [];
 
-//var drinkIngredientArray = []; // push into this
-//var drinkMeasurementArray = []; // push into this, render()
+var drinkTitle = $("#drinkTitleEl");
+var recipe = $("#recipeList");
+var instructions = $("#instructions");
+var drinkImgDiv = $("#drinkImgDiv");
+var ingredientHeader = $('#ingredientsHeader');
+var instructionHeader = $('#insructionsHeader')
+var drinkCol = $('#drinkCol');
 
-var image; // = response.drinks.strDrinkThumb; // move this to the ajax call
 
-//All of these variables to be included in a one <div>
-var drinkTitle = $("#drinkTitleEl");//.append(drinkName).text(); // defined in API as strDrink
-var recipe = $("#recipeListEl");//.append(measurements + ingredients).text(); // defined in API as strMeasure# (measurements) response.+ strIngredient# (ingredients).  The null values will be thrown out ...  but how?
-var instructions = $("#instructionsEl"); //.append(drinkInstructions).text();
-
-//The img variable will have a separate <div>
-var img = $("#drinkImgEl");//.append("#imgDiv"); // defined as strDrinkThumb in the API.  What do we type after #imgDiv to run apply the image?
 
 var decider;
 
+function loadDrinkStorage(){
+    savedDrinkArr = JSON.parse(localStorage.getItem('drinkArr'));
+    if(savedDrinkArr === null) {
+        saveBookArr = [];
+    }
+    console.log(savedDrinkArr);
+}
 
-var searchBtn = $('#search-btn');
 
 function drinkCalls() {
+    saveDrinkBtn.toggle();
     if (containsAlcohol) {
         decider = alcoholic;
     } else {
@@ -50,18 +54,28 @@ function drinkCalls() {
             url: "https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=" + drinkIdNumber,
             method: "GET"
         }).then(function (response) {
+            if (drinkImg !== undefined) {
+                drinkImg.remove();
+            }
             drinkName = response.drinks[0].strDrink;
             drinkInstructions = response.drinks[0].strInstructions;
-
-            image = response.drinks[0].strDrinkThumb;
-            drinkTitle.text(drinkName); 
+            drinkImg = $('<img>');
+            drinkUrl = response.drinks[0].strDrinkThumb;
+            drinkImg.attr('src', drinkUrl);
+            drinkImg.attr('id', 'drinkImg');
+            drinkImgDiv.append(drinkImg);
+            drinkTitle.text(drinkName);
             instructions.text(drinkInstructions);
-            img.attr("src", image);
+            ingredientHeader.text('Ingredients:');
+            instructionHeader.text('Instructions:');
+
+
 
             ingredients = [response.drinks[0].strIngredient1, response.drinks[0].strIngredient2, response.drinks[0].strIngredient3, response.drinks[0].strIngredient4, response.drinks[0].strIngredient5, response.drinks[0].strIngredient6, response.drinks[0].strIngredient7, response.drinks[0].strIngredient8, response.drinks[0].strIngredient9, response.drinks[0].strIngredient10, response.drinks[0].strIngredient11, response.drinks[0].strIngredient12, response.drinks[0].strIngredient13, response.drinks[0].strIngredient14, response.drinks[0].strIngredient15];
 
             measurements = [response.drinks[0].strMeasure1, response.drinks[0].strMeasure2, response.drinks[0].strMeasure3, response.drinks[0].strMeasure4, response.drinks[0].strMeasure5, response.drinks[0].strMeasure6, response.drinks[0].strMeasure7, response.drinks[0].strMeasure8, response.drinks[0].strMeasure9, response.drinks[0].strMeasure10, response.drinks[0].strMeasure11, response.drinks[0].strMeasure12, response.drinks[0].strMeasure13, response.drinks[0].strMeasure14, response.drinks[0].strMeasure15];
 
+            recipe.empty();
             let ingredientList = $("<ul>");
             for (let i = 0; i < ingredients.length; i++) {
                 if (ingredients[i] !== null) {
@@ -73,6 +87,8 @@ function drinkCalls() {
                 }
             }
             recipe.append(ingredientList);
+            saveDrinkBtn.toggle();
+           
         });
     });
 
---
+++ drinkList.js
@@ -0,0 +1,107 @@
+//save the drink... make an object... put in an aray... save the array
+var drinkArr;
+
+var savedDrinkContainer = $('#savedDrinkContainer');
+
+saveDrinkBtn.on('click', createDrinkObject);
+barCartBtn.on('click', gotoBarcart);
+
+function createDrinkObject() {
+    if (savedDrinkArr.length !== 0) {
+        let containsDrink = false;
+        savedDrinkArr.forEach(drink => {
+            if (drink.drName === drinkName) {
+                containsDrink = true;
+            }
+        });
+        if (!containsDrink) {
+            savedDrinkArr.push({
+                drName: drinkName,
+                drInstructions: drinkInstructions,
+                drIngredients: ingredients,
+                drMeasurements: measurements,
+                drImgURL: drinkUrl
+            });
+        }
+    } else {
+        savedDrinkArr.push({
+            drName: drinkName,
+            drInstructions: drinkInstructions,
+            drIngredients: ingredients,
+            drMeasurements: measurements,   
+            drImgURL: drinkUrl
+        });
+    }
+    //store the object in local storage
+    localStorage.setItem('drinkArr', JSON.stringify(savedDrinkArr));
+
+}
+
+function gotoBarcart(event){
+    event.preventDefault();
+    window.location.href = 'savedDrinks.html';   
+}
+
+function renderBarCart() {
+    drinkArr = JSON.parse(localStorage.getItem('drinkArr'));
+    console.log(drinkArr);
+
+    //picture     name     ingredients       instructions
+    
+    if(drinkArr === null || drinkArr.length === 0) {
+        //try a modal alerting that there's nothing on the barcart, go to the top shelf
+        console.log('test');
+        $('#myDrinkModal').modal('show');
+
+    } else {
+        //loop through array and render elements
+        drinkArr.forEach(drink => {
+            //create row
+            let row = $('<div>').attr('class', 'row barCartRow');
+            //create 4 cols
+            let imgCol = $('<div>').attr('class', 'col-md-2');
+            let barCartImg = $('<img>');
+            let nameCol = $('<div>').attr('class', 'col-md-2');
+            let nameHeader = $('<h5>').attr('classl', 'barCartName');
+            let ingredientsCol = $('<div>').attr('class', 'col-md-3');
+            let instructionsCol = $('<div>').attr('class', 'col-md-3');
+            let removeCol = $('<div>').attr('class', 'col-md-2');
+
+            barCartImg.attr('src', drink.drImgURL);
+            barCartImg.attr('class', 'barCartDrink');
+            imgCol.append(barCartImg);
+            row.append(imgCol);
+            nameHeader.text(drink.drName);
+            nameCol.append(nameHeader);
+            row.append(nameCol);
+
+            let ingredientList = $("<ul>");
+            for (let i = 0; i < drink.drIngredients.length; i++) {
+                if (drink.drIngredients[i] !== null) {
+                    if (drink.drMeasurements[i] === null) {
+                        ingredientList.append($("<li>").text(drink.drIngredients[i]));
+                    } else {
+                        ingredientList.append($("<li>").text(drink.drMeasurements[i] + " " + drink.drIngredients[i]));
+                    }
+                }
+            }
+            ingredientsCol.append(ingredientList);
+            row.append(ingredientsCol);
+            instructionsCol.text(drink.drInstructions);
+            row.append(instructionsCol);
+
+            let removeBtn = $('<button>').attr('class', 'btn btn-dark m1-2 my-sm-0 barCartBtn');
+            removeBtn.text('Remove');
+            removeCol.append(removeBtn);
+            row.append(removeCol);
+
+            $('#savedDrinkContainer').append(row);
+       });
+        
+   }
+
+
+
+
+       
+}
